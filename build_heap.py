051685df5f3186f2a539247386e6f1390c5ffa76
dserveroriginal <60070115+dserveroriginal@users.noreply.github.com>
2023-03-19 18:19:06 +0200

    221RDB047

--- build_heap.py
+++ build_heap.py
@@ -1,31 +1,60 @@
-# python3
+# python3 221RDB047 
 
+def check_heap(heap):
+    for index in range(len(heap)-1,0,-1):
+        if heap[index]<heap[index//2]:
+            return index
+    return -1
+
+def swap_with_parent(heap,index):
+    heap[index],heap[index//2]=heap[index//2],heap[index]
+    return heap
+
+def pr(heap):
+    for i in range(1,len(heap)):
+        print(heap[i],end=" ")
+    print()
 
 def build_heap(data):
     swaps = []
+    
+    heap=[0]
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-
-
+    for index in range(len(data)):
+        heap.append(data[index])
+    index=check_heap(heap)
+    while not check_heap(heap)==-1:
+        current_index=index
+        index=index-1
+        while heap[current_index]<heap[current_index//2]:
+            heap=swap_with_parent(heap,current_index)
+            swaps.append((current_index//2-1,current_index-1))
+            # print(current_index//2-1,"<->",current_index-1)
+            current_index=current_index//2
+            # pr(heap)
+    
     return swaps
 
 
 def main():
     
     # TODO : add input and corresponding checks
-    # add another input for I or F 
+    # add another input for I or F 5
+    # 5 4 3 2 1
     # first two tests are from keyboard, third test is from a file
-
+    type=input()
+    lenght=int(input())
 
     # input from keyboard
-    n = int(input())
     data = list(map(int, input().split()))
 
     # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
+    # assert len(data) == lenght
 
     # calls function to assess the data 
     # and give back all swaps
+    
     swaps = build_heap(data)
 
     # TODO: output how many swaps were made, 
